568c71f28528b7349e96fd2f6ae223f1432164fe
Sysco-CH <szilvia.szalko@outlook.com>
2023-12-01 14:37:54 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = `28fba3f5-b4da-48c0-b34f-4352cd99f139`;
+export const MODEL_CLOSE_SEC = 2.5;
